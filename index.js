f0ba41e1439fc9b71a71d11335206978e52a2ed1
Marius Andra <marius.andra@gmail.com>
2016-05-17 23:55:07 +0200

    0.2.4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function camelize (str) {
-  return str.replace('_', ' ').replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
+  return str.replace(/_/g, ' ').replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
     return index === 0 ? letter.toLowerCase() : letter.toUpperCase()
   }).replace(/\s+/g, '')
 }
